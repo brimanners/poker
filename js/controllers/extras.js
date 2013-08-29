9cc9c4d391d854d2de7791b838ff13141b8cb986
Brian Manners <brimanners@yahoo.co.uk>
2013-08-29 15:21:33 +1000

    Extra pages to generate dropdowns via angular controller reading relevent json files

---
+++ js/controllers/extras.js
@@ -0,0 +1,38 @@
+var extras = angular.module('extras', []);
+
+extras.controller('extrasController', function ($scope, $http) {
+   var extraDetails = {};
+
+   $http.get('../json/event-history.json').success(function (data) {
+           extraDetails.events = data;
+           getUrlForEvents(extraDetails.events);
+   });
+
+   $http.get('../json/current-table.json').success(function (data) {
+      extraDetails.results = data;
+      getUrlsForPlayers(extraDetails.results);
+   });
+
+   $http.get('../json/extras.json').success(function (data) {
+         extraDetails.extras = data;
+   });
+
+    $scope.extraDetails = extraDetails;
+});
+
+
+function getUrlForEvents(events) {
+    for (var i=0; i < events.length; i++) {
+        var eventDate = events[i].eventDate
+        events[i].url =  "../events/" + eventDate.substring(6) + eventDate.substring(3,5) + eventDate.substring(0,2) + ".html";
+    }
+    return events;
+}
+
+function getUrlsForPlayers(players) {
+    for (var i=0; i < players.length; i++) {
+        var nameParts = players[i].name.split(" ");
+        players[i].url = "../players/" + nameParts[0].toLowerCase() + nameParts[1] + ".html";
+    }
+    return players;
+}
