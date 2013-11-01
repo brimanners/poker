d52c602032573ef942d8577fd0ccb4b58a7f37eb
Brian Manners <brimanners@yahoo.co.uk>
2013-11-01 12:50:14 +1100

    Display ladder movement on home page.

--- js/controllers/homePage.js
+++ js/controllers/homePage.js
@@ -1,3 +1,5 @@
+var playerEventPosition;
+
 var ladderModule = angular.module('poker', []);        // poker module is the name of the ng-app on the template
 
 ladderModule.controller('homePageController', function ($scope, $http) {
@@ -39,10 +41,21 @@ function populateDetailsFromJson ($scope, $http)  {
 
     $http.get('json/current-table.json').success(function (data) {
         noOfTourneys = data.length;
-        $scope.eventId = noOfTourneys;
+        playerEventPosition = calculatePlayerMovement(data);
+        $scope.eventId = data.length;
         statistics.players = data[0]["event" + noOfTourneys];
         for (i = 0; i < noOfTourneys; i ++) {
              eventTables[i + 1] = data[noOfTourneys - 1 - i]["event" + (i + 1)];
+             for (j = 0; j < eventTables[i + 1].length; j ++) {
+                var movement = 0;
+                if (i > 0) {
+                     var checking =  (i + 1) + ":" + eventTables[i + 1][j].name;
+                     ladderMovement = getLadderMovement((i + 1) + ":" + eventTables[i + 1][j].name) -
+                                getLadderMovement(i + ":" + eventTables[i + 1][j].name);
+                     var blah2 = "";
+                     eventTables[i + 1][j].movement = ladderMovement;
+                }
+             }
              statistics.eventTables = eventTables;
          }
          $scope.displayTable = eventTables[noOfTourneys];
@@ -55,7 +68,7 @@ function populateDetailsFromJson ($scope, $http)  {
 
     $http.get('json/form-table.json').success(function (data) {
             $scope.formTable = data;
-            getUrlsForPlayers(data)
+            getUrlsForPlayers(data);
     });
 
 
@@ -90,4 +103,72 @@ function stripUrlPrefix(extras) {
     for (var i=0; i < extras.length; i++) {
         extras[i].url = extras[i].url.replace("../","");
     }
+}
+
+function calculatePlayerMovement(playerData) {
+
+    var playerEventPosition = {};                   // This is the hashmap (via an object) that we can return
+    for (var i=0; i < playerData.length; i++) {
+        var playerLadderData = new Object();
+        var eventNumber = playerData.length - i;
+        var eventPlayerPosition = new Array();
+        for (j=0; j < playerData[i]["event" + (playerData.length - i)].length; j++) {
+            var check = playerData[i]["event" + (playerData.length - i)][j].name;
+            getUnsortedLadderForEvent(playerLadderData, playerData[i]["event" + (playerData.length - i)][j]);
+        }
+        var sortedLadder = sortEventData(playerLadderData);
+        for (k=0; k < sortedLadder.length; k++) {
+            playerEventPosition[eventNumber + ":" + sortedLadder[k].name] = (k + 1);
+        }
+    }
+     return playerEventPosition;
+}
+
+function getUnsortedLadderForEvent(playerLadderData, playerData) {
+    player = new Object();
+    player.name = playerData.name;
+    player.played = playerData.played;
+    player.won = playerData.won
+    player.points = playerData.points
+
+    playerLadderData[player.name] = player;
+
+}
+
+function sortEventData(playerLadderData) {
+
+   // get array from map - then sort that
+   var players = new Array();
+   $.each(playerLadderData, function(index, player) {
+       players.push(player);
+   });
+
+   players.sort(function(playerA, playerB) {
+        a = playerA;
+        b = playerB;
+        if (a.points !== b.points) {
+            return sortReturn(a.points, b.points);
+        } else if (a.won !== b.won) {
+            return sortReturn(a.won, b.won);
+        } else if (a.played !== b.played) {
+            return sortReturn(b.played, a.played);
+        } else if (a.average !== b.average) {
+			return sortReturn(a.average, b.average);
+		} else {
+			return sortReturn(b.name, a.name);
+        }
+   });
+   return players;
+}
+
+function sortReturn(compare1, compare2) {
+    if (compare1 < compare2) {
+        return 1
+    }
+    return -1
+}
+
+
+function getLadderMovement(playerEvent) {
+    return playerEventPosition[playerEvent];
 }
