e328eaf67a9fb54c6fd4ad2f00e973539b7272b1
Brian Manners <brimanners@yahoo.co.uk>
2017-12-27 12:33:23 +1100

    Creating templates for 2018!

--- js/controllers/menusController.js
+++ js/controllers/menusController.js
@@ -30,20 +30,20 @@ app.controller('menusController', ['$scope', '$http', function($scope, $http) {
          $scope.statistics.CCXIII_Events = data;
   });
 
+  $http.get('../json/2014/event-history.json').success(function (data) {
+         for (i = 0; i < data.length; i ++) { // append year so menu dropdown can section values
+             data[i].year = data[i].eventDate.substring(data[i].eventDate.length - 4, data[i].eventDate.length);
+         }
+         getUrlForEvents2(data);
+         $scope.statistics.CCXIV_Events = data;
+     });
+
    $http.get('../json/2015/event-history.json').success(function (data) {
        for (i = 0; i < data.length; i ++) { // append year so menu dropdown can section values
            data[i].year = data[i].eventDate.substring(data[i].eventDate.length - 4, data[i].eventDate.length);
        }
        getUrlForEvents2(data);
        $scope.statistics.CCXV_Events = data;
-   });
-
-   $http.get('../json/2014/event-history.json').success(function (data) {
-       for (i = 0; i < data.length; i ++) { // append year so menu dropdown can section values
-           data[i].year = data[i].eventDate.substring(data[i].eventDate.length - 4, data[i].eventDate.length);
-       }
-       getUrlForEvents2(data);
-       $scope.statistics.CCXIV_Events = data;
    });
 
   $http.get('../json/2016/event-history.json').success(function (data) {
@@ -59,8 +59,17 @@ app.controller('menusController', ['$scope', '$http', function($scope, $http) {
           data[i].year = data[i].eventDate.substring(data[i].eventDate.length - 4, data[i].eventDate.length);
         }
         getUrlForEvents2(data);
-        $scope.statistics.events = data;
+       $scope.statistics.CCXVII_Events = data;
    });
+
+   $http.get('../json/2018/event-history.json').success(function (data) {
+           for (i = 0; i < data.length; i ++) { // append year so menu dropdown can section values
+             data[i].year = data[i].eventDate.substring(data[i].eventDate.length - 4, data[i].eventDate.length);
+           }
+           getUrlForEvents2(data);
+           $scope.statistics.events = data;
+      });
+
 
    $http.get('../json/general/extras.json').success(function (data) {
       $scope.statistics.extras = data;
